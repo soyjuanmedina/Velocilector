ab99f62f9a8eca2d2b067d3c9c018bd7041f7aa2
Juan Medina <soyjuanmedina@gmail.com>
2024-07-12 16:52:13 +0200

    'New_front'

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { HomeComponent } from './pages/home/home.component'
 import { HowWorksComponent } from './pages/how-works/how-works.component';
 import { ReadComponent } from './pages/read/read.component';
 import { ReadTestComponent } from './pages/read-test/read-test.component';
+import { PrivacyComponent } from './pages/privacy/privacy.component';
 
 export const routes: Routes = [
   { path: '', title: "Velocilector", component: HomeComponent },
+  { path: 'privacy', title: "Velocilector - Privacidad", component: PrivacyComponent },
   { path: 'read', component: ReadComponent },
   { path: 'read-test', component: ReadTestComponent },
   { path: 'how-works', component: HowWorksComponent },
